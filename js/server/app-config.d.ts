26cc43e3525c70b826e04f9838ba469d6fc3412c
wchang28 <wchang28@hotmail.com>
2017-07-07 17:42:50 -0400

    first commit

---
+++ js/server/app-config.d.ts
@@ -0,0 +1,4 @@
+import { IWebServerConfig } from 'express-web-server';
+export interface IAppConfig {
+    webServerConfig: IWebServerConfig;
+}
--- js/server/app.js
+++ js/server/app.js
@@ -1,55 +1,43 @@
-import * as express from 'express';
-import {IWebServerConfig, startServer} from 'express-web-server';
-import * as bodyParser from "body-parser";
-import noCache = require('no-cache-express');
-import * as prettyPrinter from 'express-pretty-print';
-import * as fs from 'fs';
-import * as path from 'path';
-import {IAppConfig} from './app-config';
-
-let config: IAppConfig = null;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var express_web_server_1 = require("express-web-server");
+var bodyParser = require("body-parser");
+var noCache = require("no-cache-express");
+var prettyPrinter = require("express-pretty-print");
+var fs = require("fs");
+var config = null;
 if (process.argv.length < 3) {
     console.error("config file not optional!");
     process.exit(1);
 }
-
 config = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
-
-let app = express();
-
+var app = express();
 app.set('jsonp callback name', 'cb');
-
 app.use(noCache);
-app.use(bodyParser.text({"limit":"999mb"}));
-app.use(bodyParser.json({"limit":"999mb"}));
+app.use(bodyParser.text({ "limit": "999mb" }));
+app.use(bodyParser.json({ "limit": "999mb" }));
 app.use(prettyPrinter.get());
-
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use(function (req, res, next) {
     console.log("\n" + new Date().toISOString() + ': method=' + req.method + ', url=' + req.url + "\nheaders=\n" + JSON.stringify(req.headers, null, 2));
     next();
 });
-
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-
-app.options("/*", (req: express.Request, res: express.Response) => {
+app.options("/*", function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH,HEAD');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Content-Length,X-Requested-With');
     res.send(200);
 });
-
 //app.set("global", g);
-
 //app.use('/services', servicesRouter);
-
-startServer(config.webServerConfig, app, (secure:boolean, host:string, port:number) => {
-    let protocol = (secure ? 'https' : 'http');
+express_web_server_1.startServer(config.webServerConfig, app, function (secure, host, port) {
+    var protocol = (secure ? 'https' : 'http');
     console.log(new Date().toISOString() + ': system integration server listening at %s://%s:%s', protocol, host, port);
-}, (err:any) => {
+}, function (err) {
     console.error(new Date().toISOString() + ': !!! system integration server error: ' + JSON.stringify(err));
     process.exit(1);
 });
