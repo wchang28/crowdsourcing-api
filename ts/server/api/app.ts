98380b6fd5006a852b81315ebac7d22754354a49
wchang28 <wchang28@hotmail.com>
2017-07-11 16:29:31 -0400

    bnrtsnbrst

--- ts/server/api/app.ts
+++ ts/server/api/app.ts
@@ -38,10 +38,11 @@ appFactory.on("app-just-created", (app: express.Express) => {
 
 let app = appFactory.create();
 
+console.log(new Date().toISOString() + ": connecting to the msg server...");
 let api = new rcf.AuthorizedRestApi(node$.get(), {instance_url: "http://127.0.0.1:" + MsgPort.toString()});
 let msgClient = api.$M("/msg/events/event_stream", {reconnetIntervalMS: 3000});
 msgClient.on("connect", (conn_id: string) => {
-    console.log(new Date().toISOString() + ": connected :-) conn_id=" + conn_id);
+    console.log(new Date().toISOString() + ": connected to the msg server :-) conn_id=" + conn_id);
     msgClient.subscribe("/topic/" + InstanceId, (msg: rcf.IMessage) => {
         if (msg.body) {
             let message : Message = msg.body;
