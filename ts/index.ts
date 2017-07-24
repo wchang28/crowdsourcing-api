8c8fba15480c91f741f5df17b12634bd7c784729
wchang28 <wchang28@hotmail.com>
2017-07-24 15:36:13 -0400

    indenting

--- ts/index.ts
+++ ts/index.ts
@@ -5,11 +5,11 @@ import {AppGlobal} from "./app-global";
 export {Request, Response, NextFunction, Router, RequestHandler} from "express";
 export * from "rcf";
 export {AppGlobal, CGIChildProcessLauncher} from "./app-global";
-export {ExecOptions} from  "child_process";
+export {ExecOptions} from "child_process";
 
 export interface ExtensionModule {
-	module: string;
-	homePath: string;
+    module: string;
+    homePath: string;
     package_json: any;
     README_md?: string;
 }
